81bde3ac169b5bfaf8eea9bb220aec260a4918b4
First Test <rajbir.s.bains@gmail.com>
2021-05-06 14:23:17 -0700

    Initial survey commit

---
+++ app/src/main/java/com/example/hrtfitnessapp/SurveyStart.java
@@ -0,0 +1,14 @@
+package com.example.hrtfitnessapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SurveyStart extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_survey_start);
+    }
+}
