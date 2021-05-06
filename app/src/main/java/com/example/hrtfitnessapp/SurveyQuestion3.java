71ba297589b03d7204a8908ac8c45cfb83098af0
First Test <rajbir.s.bains@gmail.com>
2021-05-05 23:18:52 -0700

    Add survey activities

---
+++ app/src/main/java/com/example/hrtfitnessapp/SurveyQuestion3.java
@@ -0,0 +1,14 @@
+package com.example.hrtfitnessapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SurveyQuestion3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_survey_question3);
+    }
+}
---
+++ app/src/main/java/com/example/hrtfitnessapp/SurveyQuestion4.java
@@ -0,0 +1,14 @@
+package com.example.hrtfitnessapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SurveyQuestion4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_survey_question4);
+    }
+}
