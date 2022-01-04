cb10fe66b956da824a22a3a921f6c9d584513e98
Tej Pooni <tejpooni@tejs-mbp-2.lan>
2022-01-03 19:56:41 -0800

    added tracker use case

--- app/src/main/java/com/example/hrtfitnessapp/PostSurvey.java
+++ app/src/main/java/com/example/hrtfitnessapp/PostSurvey.java
@@ -7,8 +7,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class PostSurvey extends AppCompatActivity {
     private Button toHRTButton;
+    private DatabaseReference ref;
+    private DatabaseReference liftsAndMacros;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,14 +27,28 @@ public class PostSurvey extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 OpenNavScreenActivity();
+
+                ref = FirebaseDatabase.getInstance().getReference("User").child(FirebaseAuth.getInstance().getCurrentUser().getUid());
+                liftsAndMacros = ref.child("Tracker");
+
+                liftsAndMacros.child("squat").setValue(0);
+                liftsAndMacros.child("bench").setValue(0);
+                liftsAndMacros.child("deadlift").setValue(0);
+                liftsAndMacros.child("fat").setValue(0);
+                liftsAndMacros.child("carbs").setValue(0);
+                liftsAndMacros.child("protein").setValue(0);
+
             }
         });
 
 
     }
 
+
     public void OpenNavScreenActivity(){
         Intent intent1 = new Intent(this,NavScreen.class);
         startActivity(intent1);
+
+
     }
 }
